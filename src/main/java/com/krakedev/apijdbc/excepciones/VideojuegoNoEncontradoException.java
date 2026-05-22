2e7f22b03d469b39de037cd8ba51274df46a0e13
Ana62-mp <ana.moposita84@gmail.com>
2026-05-21 19:53:49 -0500

    APMP: Creacion de excepcion para videojuego no encontrado

---
+++ src/main/java/com/krakedev/apijdbc/excepciones/VideojuegoNoEncontradoException.java
@@ -0,0 +1,7 @@
+package com.krakedev.apijdbc.excepciones;
+
+public class VideojuegoNoEncontradoException extends RuntimeException {
+	public VideojuegoNoEncontradoException(String m) {
+		super(m);
+	}
+}
