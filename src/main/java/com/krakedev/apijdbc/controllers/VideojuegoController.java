23a77927c16984f5c3edabd919e82657f3c63c65
Ana62-mp <ana.moposita84@gmail.com>
2026-05-22 10:46:11 -0500

    APMP: Creacion metodos actualizar y eliminar, pruba en postman exitosa

--- src/main/java/com/krakedev/apijdbc/controllers/VideojuegoController.java
+++ src/main/java/com/krakedev/apijdbc/controllers/VideojuegoController.java
@@ -2,9 +2,11 @@ package com.krakedev.apijdbc.controllers;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -69,4 +71,29 @@ public class VideojuegoController {
             return new ResponseEntity<>(er, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+    
+    @PutMapping("/{codigo}")
+    public ResponseEntity<?> actualizar(@PathVariable String codigo, @RequestBody Videojuego videojuego) {
+        try {
+            service.actualizar(codigo, videojuego);
+            return ResponseEntity.ok("Videojuego actualizado correctamente");
+
+        } catch (RuntimeException ex) {
+            ErrorResponse er = new ErrorResponse(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR.value());
+            return new ResponseEntity<>(er, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    
+    @DeleteMapping("/{codigo}")
+    public ResponseEntity<?> eliminar(@PathVariable String codigo) {
+        try {
+            return ResponseEntity.ok(service.eliminar(codigo));
+
+        } catch (RuntimeException ex) {
+            ErrorResponse er = new ErrorResponse(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR.value());
+            return new ResponseEntity<>(er, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    
+    
 }
